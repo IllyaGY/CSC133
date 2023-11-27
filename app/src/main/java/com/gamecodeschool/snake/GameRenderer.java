717adbfe66ed4f9a2dfebb3fbf7d83e9de833b5a
prabhas1208 <125933378+prabhas1208@users.noreply.github.com>
2023-11-26 21:08:18 -0800

    Background color changed for GameOver Screen

--- app/src/main/java/com/gamecodeschool/snake/GameRenderer.java
+++ app/src/main/java/com/gamecodeschool/snake/GameRenderer.java
@@ -41,7 +41,7 @@ public class GameRenderer extends SurfaceView {
             drawGameOverScreen();
         } else {
             // Draw the score
-            paint.setColor(Color.WHITE); // Example color, adjust as needed
+            paint.setColor(Color.WHITE);
             paint.setTextSize(120);
             canvas.drawText("Score: " + score, 20, 120, paint);
 
@@ -65,9 +65,9 @@ public class GameRenderer extends SurfaceView {
     }
 
     private void drawGameOverScreen() {
-        paint.setColor(Color.argb(100, 160, 24, 24)); // Semi-transparent black background
+        paint.setColor(Color.argb(160, 160, 24, 24)); // Semi-transparent black background
         canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
-        paint.setColor(Color.RED); // Example color
+        paint.setColor(Color.RED);
         paint.setTextSize(250);
         canvas.drawText("Game Over", 200, 400, paint);
         canvas.drawText("Score: " + score, 200, 700, paint);
@@ -79,7 +79,7 @@ public class GameRenderer extends SurfaceView {
         paint.setColor(Color.argb(100, 0, 0, 0)); // Semi-transparent black background
         canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
 
-        paint.setColor(Color.YELLOW); // Example color
+        paint.setColor(Color.YELLOW);
         paint.setTextSize(180);
         canvas.drawText("Resume, Press R", 200, 500, paint);
         paint.setTextSize(160);
